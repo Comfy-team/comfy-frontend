5d86688041e7ef2a02a2346e366a50ddd84f7a29
aya <aya.saeed126@gmail.com>
2023-06-14 15:15:14 +0300

    update custom multi-slider arrows

--- src/components/common/customSliderArrows.jsx
+++ src/components/common/customSliderArrows.jsx
@@ -1,8 +1,4 @@
 export const RightArrow = ({ onClick, ...rest }) => {
-  const {
-    onMove,
-    carouselState: { currentSlide, deviceType },
-  } = rest;
   return (
     <button
       className="btn slider-btn text-white react-multiple-carousel__arrow react-multiple-carousel__arrow--right"
@@ -12,10 +8,6 @@ export const RightArrow = ({ onClick, ...rest }) => {
 };
 
 export const LeftArrow = ({ onClick, ...rest }) => {
-  const {
-    onMove,
-    carouselState: { currentSlide, deviceType },
-  } = rest;
   return (
     <button
       className="btn slider-btn text-white react-multiple-carousel__arrow react-multiple-carousel__arrow--left"
