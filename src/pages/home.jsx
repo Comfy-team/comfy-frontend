488d458dbf6cb68a10fd542ec9297e1f3992c9eb
aya <aya.saeed126@gmail.com>
2023-06-14 15:31:54 +0300

    CS-58 add instagarm slider to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,12 @@
+// components
+import FollowUs from "./../components/common/followUs";
+
 const Home = () => {
-    return ( <div id="home">home</div> );
-}
- 
+  return (
+    <div id="home">
+      <FollowUs />
+    </div>
+  );
+};
+
 export default Home;
