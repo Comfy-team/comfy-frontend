741b0d0815c68cb4dd011b61aab86470d0b67411
aya <aya.saeed126@gmail.com>
2023-06-14 15:33:25 +0300

    CS-59 add brands slider to home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 // components
+import Brands from "../components/common/brands";
 import FollowUs from "./../components/common/followUs";
 
 const Home = () => {
   return (
     <div id="home">
       <FollowUs />
+      <Brands />
     </div>
   );
 };
