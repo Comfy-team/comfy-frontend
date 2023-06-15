4d14a8aaceb985a25e2702516890ecd7bef308f4
Amalhalim <amalhalim333@gmail.com>
2023-06-16 00:35:47 +0300

    upload pageHeader file

--- src/components/layouts/layoutWithPageHeader.jsx
+++ src/components/layouts/layoutWithPageHeader.jsx
@@ -2,11 +2,11 @@ import { Outlet } from "react-router-dom";
 
 // components
 import PageHeader from "../common/pageHeader";
-
+const pages = { label: "example page", href: "/" };
 const LayoutWithPageHeader = () => {
   return (
     <>
-      <PageHeader />
+      <PageHeader pages={pages} />
       <Outlet />
     </>
   );
