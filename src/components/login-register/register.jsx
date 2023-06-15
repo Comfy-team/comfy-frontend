40e9f235e8ba7bd5638f9a48e567d60a7bb56e68
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-06-15 20:14:48 +0300

    CS-52 update register form validation

--- src/components/login-register/register.jsx
+++ src/components/login-register/register.jsx
@@ -63,7 +63,7 @@ const Register = ({onRegistrationSuccess}) => {
             .min(8, "Password must be at least 8 characters long")
             .matches(
               /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol."
+              "Password must contain at least one uppercase letter, one lowercase letter, one number, and one symbol from (@$!%*?&) ."
             ),
           confirmPassword: Yup.string()
             .required("Confirm Password is required")
