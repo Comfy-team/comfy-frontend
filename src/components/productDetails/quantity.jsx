68a4af451be3ffbf6bebf3702015a882c279757d
aya <aya.saeed126@gmail.com>
2023-06-19 02:02:50 +0300

    update product details

--- src/components/productDetails/quantity.jsx
+++ src/components/productDetails/quantity.jsx
@@ -2,14 +2,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquarePlus, faSquareMinus } from "@fortawesome/free-solid-svg-icons";
 
-const Quantity = ({ active, stock, onQuantityChange }) => {
+const Quantity = ({ id, active, stock, onQuantityChange, onDeleteItem }) => {
   return (
     <div className="d-flex justify-content-center align-items-center gap-3">
       <button
         type="button"
         className="btn fs-4 p-0 border-0 outline-0 color-main-black hover-color-yellow"
         disabled={active === stock ? true : false}
-        onClick={() => onQuantityChange(active+1)}
+        onClick={() => onQuantityChange(id, active + 1)}
       >
         <FontAwesomeIcon icon={faSquarePlus} />
         <span aria-hidden className="visually-hidden">
@@ -20,8 +20,10 @@ const Quantity = ({ active, stock, onQuantityChange }) => {
       <button
         type="button"
         className="btn fs-4 p-0 border-0 outline-0 color-main-black hover-color-yellow"
-        disabled={active === 1 ? true : false}
-        onClick={() => onQuantityChange(active-1)}
+        disabled={active === 0 ? true : false}
+        onClick={() =>
+          active === 1 ? onDeleteItem(id) : onQuantityChange(id, active - 1)
+        }
       >
         <FontAwesomeIcon icon={faSquareMinus} />
         <span aria-hidden className="visually-hidden">
