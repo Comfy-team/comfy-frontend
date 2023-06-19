3a5ef72574730c0d302a09b679a45e1c2e9273ce
aya <aya.saeed126@gmail.com>
2023-06-19 12:14:39 +0300

    added login modal

--- src/components/layouts/layoutWithNav.jsx
+++ src/components/layouts/layoutWithNav.jsx
@@ -1,16 +1,20 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 
 // components
 import Header from "../common/header";
 import Footer from "../common/footer";
+import Modal from "../login-register/modal";
 
 // style
 import style from "./layouts.module.css";
 
 const LayoutWithNav = () => {
+  const showLoginModal = useSelector((state) => state.loginModal.show);
   return (
     <>
       <Header />
+      {showLoginModal && <Modal />}
       <div className={style.content}>
         <Outlet />
       </div>
