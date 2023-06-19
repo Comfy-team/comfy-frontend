a2e64afe87ecb882812398546f25a8f9c9d1c403
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-06-19 23:24:55 +0300

    CS-81 add page 404

--- src/pages/page404/index.jsx
+++ src/pages/page404/index.jsx
@@ -1,5 +1,21 @@
+import styles from "./page404.module.css";
+
 const Page404 = () => {
-    return ( <div id="page404">404</div> );
-}
- 
+  return (
+    <div id="page404" className={`${styles.Page404}`}>
+      
+        <div className="text-center">
+          <h1 className={`${styles["text-9xl"]} ${styles["font-bold"]}`}>404</h1>
+          <h1 className="fw-bolder">Page Not Found</h1>
+          <p>we couldn't find what you were looking for.</p>
+          <p>
+            Please contact the owner of the site that linked you to the original
+            URl and let them know their link is broken.
+          </p>
+        </div>
+      
+    </div>
+  );
+};
+
 export default Page404;
