c536f0e2239f21b56bdfd55e071ed6a6e2e248de
mariam-abdallah <mariam.m.r.ali@gmail.com>
2023-06-20 06:40:05 +0300

    CS-56 updated product cart

--- src/components/common/productCard.jsx
+++ src/components/common/productCard.jsx
@@ -1,14 +1,62 @@
-import { NavLink } from "react-router-dom";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { deleteItemFromCart } from "../../functions/cart";
 const ProductCard = ({ product }) => {
+  const [showButton, setShowButton] = useState(false);
+  const [inCart, setInCart] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  const handleMouseEnter = () => {
+    setShowButton(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowButton(false);
+  };
+  const handleDeleteFromCart = (id) => {
+    setInCart(false);
+    deleteItemFromCart(cart._id, id);
+  };
+
+  const handleAddToCart = (event) => {
+    setInCart(true);
+    event.preventDefault();
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmlhbS5tLnIuYWxpQGdtYWlsLmNvbSIsImlkIjoiNjQ4ZjBjNzNlNzE1YTBmN2FjMjY1OGU4Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE2ODcyMTE5MDAsImV4cCI6MTY4NzIzMzUwMH0.JG5YXzvsf2SvjIoZYK2PcLXSqfGi__bRLySTx7EgoD4";
+    fetch(`http://localhost:8080/cart/648f0c74e715a0f7ac2658ea`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        product_id: product._id,
+        color: `${product.color}`, 
+        price: product.price,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Product added to cart:", data);
+      })
+      .catch((error) => {
+        console.error("Error adding product to cart:", error);
+      });
+  };
+
   return (
-    <div className="product-card card position-relative border-0 rounded-0">
+    <div
+      className="product-card card position-relative border-0 rounded-0"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       {product?.discount > 0 && (
         <span className="badge d-block bg-yellow position-absolute text-white">
           -{product.discount}%
         </span>
       )}
-      <NavLink
+      <Link
         to={`/product-details/${product._id}`}
         className="holder position-relative overflow-hidden"
       >
@@ -22,15 +70,44 @@ const ProductCard = ({ product }) => {
           alt={product?.name}
           className="img-fluid card-img-top rounded-0 hover-img position-absolute w-100 h-100 top-0 start-0"
         />
-      </NavLink>
+        {/* {showButton && (
+          <button disabled={inCart}
+            className="btn btn-light w-75 p-3 border-0 addToCart-btn fw-bold position-absolute bottom-0 start-50 translate-middle"
+            onClick={handleAddToCart}
+          >
+            <FontAwesomeIcon icon={faPlus} /> Add to Cart
+          </button>
+        )}
+      {inCart && (
+        <button onClick={handleDeleteFromCart}>
+          Remove from Cart
+        </button>
+      )} */}
+      {inCart ? (
+          <button
+            className="btn btn-light w-75 p-3 border-0 addToCart-btn fw-bold position-absolute bottom-0 start-50 translate-middle"
+            onClick={() => handleDeleteFromCart(product?._id)}
+          >
+            Remove from Cart
+          </button>
+        ) : (
+          <button
+            className="btn btn-light w-75 p-3 border-0 addToCart-btn fw-bold position-absolute bottom-0 start-50 translate-middle"
+            onClick={handleAddToCart}
+            style={{ pointerEvents: !showButton ? "none" : "auto" }}
+          >
+            <FontAwesomeIcon icon={faPlus} /> Add to Cart
+          </button>
+        )}
+      </Link>
       <div className="card-body text-center">
         <h3 className="h6 fw-bold card-title">
-          <NavLink
+          <Link
             className="d-block text-ellipsis text-decoration-none hover-color-yellow color-product-name"
             to={`/product-details/${product._id}`}
           >
             {product?.name}
-          </NavLink>
+          </Link>
         </h3>
         <div className="card-text">
           {product?.discount > 0 ? (
