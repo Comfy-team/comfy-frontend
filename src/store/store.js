643b111bdc5607b47601df8161b2f672fd912420
mariam-abdallah <mariam.m.r.ali@gmail.com>
2023-06-22 07:59:40 +0300

    CS-66 upload cartModalSlice

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import brandsSlice from "./slices/brandsSlice";
 import cartSlice from "./slices/cartSlice";
 import loginModalSlice from "./slices/loginModalSlice";
+import cartModalSlice from "./slices/cartModalSlice";
 
 export default configureStore({
   reducer: {
     brands: brandsSlice,
     cart: cartSlice,
     loginModal: loginModalSlice,
+    cartModal:cartModalSlice
   },
 });
