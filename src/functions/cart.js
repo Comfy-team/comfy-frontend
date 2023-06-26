50c37b16f6579a2d1d41beba93e9e685874de794
aya <aya.saeed126@gmail.com>
2023-06-27 00:09:35 +0300

    update getCart function

--- src/functions/cart.js
+++ src/functions/cart.js
@@ -9,7 +9,7 @@ import { showLoginModal } from "../store/slices/loginModalSlice";
 export const getCart = (token) => {
   let decoded = jwt_decode(token);
   if (decoded.role === "admin") {
-    store.dispatch(setCart({ cart: {}, role: "admin" }));
+    store.dispatch(setCart({ role: "admin" }));
     return;
   }
   axiosInstance
@@ -19,7 +19,7 @@ export const getCart = (token) => {
         "x-access-token": token,
       },
     })
-    .then((res) => store.dispatch(setCart({ cart: res.data, role: "user" })))
+    .then((res) => store.dispatch(setCart({ ...res.data, role: "user" })))
     .catch((error) => console.log(error));
 };
 
