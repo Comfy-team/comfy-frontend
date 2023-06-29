13dbb5f1f6026ec8690de95548d9bc1fd608d407
aya <aya.saeed126@gmail.com>
2023-06-29 20:47:27 +0300

    add toast

---
+++ src/components/common/toast.jsx
@@ -0,0 +1,28 @@
+// react-bootstrap
+import ToastContainer from "react-bootstrap/ToastContainer";
+import Toast from "react-bootstrap/Toast";
+
+const ToastInfo = ({ msg, show, onDismissToast }) => {
+  return (
+    <ToastContainer
+      className="p-3 z-3"
+      containerPosition="fixed"
+      position="middle-center"
+    >
+      <Toast
+        bg="dark"
+        onClose={onDismissToast}
+        show={show}
+        delay={3000}
+        autohide
+      >
+        <Toast.Header className="justify-content-end bg-white">
+          <strong className="me-auto">Comfy</strong>
+        </Toast.Header>
+        <Toast.Body className="text-white">{msg}</Toast.Body>
+      </Toast>
+    </ToastContainer>
+  );
+};
+
+export default ToastInfo;
