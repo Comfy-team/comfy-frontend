548bfd8ef49bd6d31f60ba8d28b0288275d99732
aya <aya.saeed126@gmail.com>
2023-06-29 20:44:48 +0300

    add toast slice

--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,14 @@ import brandsSlice from "./slices/brandsSlice";
 import cartSlice from "./slices/cartSlice";
 import loginModalSlice from "./slices/loginModalSlice";
 import cartModalSlice from "./slices/cartModalSlice";
+import toastSlice from "./slices/toastSlice";
 
 export default configureStore({
   reducer: {
     brands: brandsSlice,
     cart: cartSlice,
     loginModal: loginModalSlice,
-    cartModal:cartModalSlice
+    cartModal: cartModalSlice,
+    toastInfo: toastSlice,
   },
 });
