685b6f567be42b1875fd2a74399c68c5aaf5fbf7
Amalhalim <amalhalim333@gmail.com>
2023-06-30 01:07:22 +0300

    add formslice of checkout page and connect it with store

---
+++ src/store/slices/formSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  theformData: {},
+  form: {},
+};
+
+const formSlice = createSlice({
+  name: "form",
+  initialState,
+  reducers: {
+    saveFormData: (state, action) => {
+      return { ...state, form: action.payload };
+    },
+  },
+});
+
+export const { saveFormData } = formSlice.actions;
+export default formSlice.reducer;
