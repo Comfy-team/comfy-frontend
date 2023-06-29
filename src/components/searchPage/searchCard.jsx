89c994e9064730b317622cef9badf7ca297ee196
aya <aya.saeed126@gmail.com>
2023-06-29 16:23:55 +0300

    CS-80 update search

--- src/components/searchPage/searchCard.jsx
+++ src/components/searchPage/searchCard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // font awesome
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
@@ -12,11 +13,14 @@ import { addItemToCart, deleteItemFromCart } from "../../functions/cart";
 import style from "../../pages/searchPage/searchPage.module.css";
 import RemoveProductWarning from "../common/removeProductWarning";
 
-const SearchCard = ({ product, cartId, inCart, query }) => {
+const SearchCard = ({ product, query }) => {
   const [shownTitle, setShownTitle] = useState("");
   const [shownCategory, setShownCategory] = useState("");
   const [shownBrand, setShownBrand] = useState("");
   const [showWarning, setShowWarning] = useState(false);
+  const [inCart, setInCart] = useState(false);
+  const [showBtnSpinner, setShowBtnSpinner] = useState(false);
+  const cart = useSelector((state) => state.cart.cart);
 
   const highlightMatch = (name, query) => {
     const regex = new RegExp(query, "i");
@@ -41,12 +45,14 @@ const SearchCard = ({ product, cartId, inCart, query }) => {
     return highlighted;
   };
 
-  const handleAddToCart = (cartId, id, color, price) => {
-    addItemToCart(cartId, id, color, price);
+  const handleAddToCart = (id, color, price) => {
+    setShowBtnSpinner(true);
+    addItemToCart(cart._id, id, color, price);
   };
 
   const handleDeleteFromCart = () => {
-    deleteItemFromCart(cartId, product._id);
+    setShowBtnSpinner(true);
+    deleteItemFromCart(cart._id, product._id);
     setShowWarning(false);
   };
 
@@ -67,15 +73,29 @@ const SearchCard = ({ product, cartId, inCart, query }) => {
     setShownBrand(highlightMatch(product.brand.name, query));
   }, [query]);
 
+  useEffect(() => {
+    setShowBtnSpinner(false);
+    const productIndx = cart.items?.findIndex(
+      (ele) => ele.product_id._id === product._id
+    );
+    setInCart(productIndx === -1 ? false : true);
+  }, [cart, product]);
+
   return (
     <>
       <div
         className={`${style["search-card"]} product-card card position-relative border-0 rounded-0`}
       >
-        {product.discount > 0 && (
+        {product.stock === 0 ? (
           <span className="badge d-block bg-yellow position-absolute text-white">
-            -{product.discount}%
+            Out Of Stock
           </span>
+        ) : (
+          product.discount > 0 && (
+            <span className="badge d-block bg-yellow position-absolute text-white">
+              -{product.discount}%
+            </span>
+          )
         )}
         <div className="position-relative overflow-hidden holder">
           <Link to={`/product-details/${product._id}`} className="d-block">
@@ -90,28 +110,36 @@ const SearchCard = ({ product, cartId, inCart, query }) => {
               className="img-fluid card-img-top rounded-0 hover-img position-absolute w-100 h-100 top-0 start-0"
             />
           </Link>
-          <button
-            type="button"
-            className={`${style["add-to-cart-btn"]} btn btn-bg-white py-2 text-uppercase position-absolute fw-semibold d-flex justify-content-center align-items-center gap-2`}
-            onClick={() =>
-              !inCart
-                ? handleAddToCart(
-                    cartId,
-                    product._id,
-                    product.colors[0].color,
-                    product.price
-                  )
-                : setShowWarning(true)
-            }
-          >
-            {inCart ? (
-              "remove from cart"
-            ) : (
-              <>
-                <FontAwesomeIcon icon={faPlus} /> <span>add to cart</span>
-              </>
-            )}
-          </button>
+          {showBtnSpinner ? (
+            <button className="add-to-cart-btn btn btn-bg-white py-2 text-uppercase position-absolute fw-semibold d-flex justify-content-center align-items-center gap-2">
+              <div className="spinner-border spinner-border-sm" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={`${style["add-to-cart-btn"]} btn btn-bg-white py-2 text-uppercase position-absolute fw-semibold d-flex justify-content-center align-items-center gap-2`}
+              onClick={() =>
+                !inCart
+                  ? handleAddToCart(
+                      product._id,
+                      product.colors[0],
+                      product.price
+                    )
+                  : setShowWarning(true)
+              }
+              disabled={product.stock > 0 ? false : true}
+            >
+              {inCart ? (
+                "remove from cart"
+              ) : (
+                <>
+                  <FontAwesomeIcon icon={faPlus} /> <span>add to cart</span>
+                </>
+              )}
+            </button>
+          )}
         </div>
         <div className="card-body text-center">
           <h3 className="h6 fw-bold card-title">
