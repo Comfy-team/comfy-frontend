c55b1536913d5303f185a668338ee37133c6e127
Amalhalim <amalhalim333@gmail.com>
2023-07-02 05:38:09 +0300

    split checkoutPage to small componant and add prefiex to css.moduel

---
+++ src/components/checkout/Breadcrumbcomponant.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../../App.css";
+import style from "../../pages/checkout/checkout.module.css";
+
+export default function Breadcrumbcomponant() {
+  return (
+    <>
+      {" "}
+      <nav
+        className={`${style.breadcrumb}  --bs-breadcrumb-divider: >`}
+        aria-label="breadcrumb"
+      >
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <Link to="/cart" className={` ${style.none} breadlink`}>
+              Cart
+            </Link>
+          </li>
+          <li className="breadcrumb-item">
+            <Link
+              to="/checkout/information"
+              className={`breadcrumb-item ${style.none}
+                  `}
+            >
+              information
+            </Link>
+          </li>
+          <li
+            className={`breadcrumb-item "
+                    }`}
+            aria-current="page"
+          >
+            shipping
+          </li>
+        </ol>
+      </nav>
+    </>
+  );
+}
