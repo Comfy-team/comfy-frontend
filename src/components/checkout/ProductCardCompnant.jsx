d8cb47531f7a68498c4e18d86a43665f3c7165c4
Amalhalim <amalhalim333@gmail.com>
2023-07-07 07:46:29 +0300

    add discount to shopping cart

--- src/components/checkout/ProductCardCompnant.jsx
+++ src/components/checkout/ProductCardCompnant.jsx
@@ -7,7 +7,7 @@ export default function ProductCardCompnant({ index, item }) {
   return (
     <>
       {" "}
-      <div key={index} className={`${style.imgcontainer} row mb-2 p-0 m-0`}>
+      <div key={item._id} className={`${style.imgcontainer} row mb-2 p-0 m-0`}>
         <div className="col-2 ">
           <img
             className={`${style.productImg}  `}
@@ -19,19 +19,26 @@ export default function ProductCardCompnant({ index, item }) {
             }`}
           />
         </div>
-
         <div className={`${style.productInfo} col-7  `}>
           <p className="mb-0 ml-3">{item.product_id.name}</p>
           <div className={`${style.quantitCircle} mb-1"`}>{item.quantity}</div>
-
           <div
-            key={index}
+            key={item.product_id}
             className={`${style.spanColor} `}
             style={{ backgroundColor: `${item.color}` }}
           ></div>
         </div>
         <div className="col-2 mt-3  ">
-          <p className="mb"> ${item.quantity * item.price}</p>
+          <p className="mb-0">
+            $
+            {item.quantity *
+              (item.price - (item.price * item.product_id.discount) / 100)}
+          </p>
+          {item.product_id.discount !== 0 && (
+            <p className={`${style.gray} mb text-decoration-line-through`}>
+              ${item.quantity * (item.quantity * item.price)}
+            </p>
+          )}
         </div>
       </div>
     </>
