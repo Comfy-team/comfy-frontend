934a8e04b76c461197992f5d0d4737c0078e4b59
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-07 19:43:20 +0300

    CS-84 update user dashboard

--- src/components/dashboard/usersDash.jsx
+++ src/components/dashboard/usersDash.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-// component
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+
+// components
 import DashPagination from "./dashPagination";
 import { showToast } from "../../store/slices/toastSlice";
 import ConfirmPopup from "../common/confirmPopup";
 import axiosInstance from "../../apis/config";
+import Spinner from "../common/spinner";
 
-//icon
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-
-//style
+// style
 import dashStyle from "./../../pages/dashboard/dashboard.module.css";
 
 const UsersDash = () => {
@@ -23,6 +24,9 @@ const UsersDash = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [userIdToDelete, setUserIdToDelete] = useState("");
   const [showWarning, setShowWarning] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(true);
+  const [displayId, setDisplayId] = useState(false);
+  const [displayEmail, setDisplayEmail] = useState(false);
   const token = localStorage.getItem("userToken");
   const dispatch = useDispatch();
 
@@ -43,6 +47,7 @@ const UsersDash = () => {
           setAllUsersInPage(res.data);
           setAllUsers(res.data.data);
           setTotaUsers(res.data.totalUsers);
+          setShowSpinner(false);
         })
         .catch((err) => {
           console.log(err);
@@ -79,7 +84,7 @@ const UsersDash = () => {
         },
       })
       .then((res) => {
-        // setDeleteStatus(`User ${id} deleted successfully.`);
+        dispatch(showToast("User was deleted successfully!"));
         axiosInstance
           .get(`/users`, {
             params: {
@@ -94,7 +99,6 @@ const UsersDash = () => {
             setAllUsersInPage(res.data);
             setAllUsers(res.data.data);
             setTotaUsers(res.data.totalUsers);
-            dispatch(showToast("User was deleted successfully!"));
             setUserIdToDelete("");
           })
           .catch((err) => {
@@ -103,13 +107,10 @@ const UsersDash = () => {
       })
       .catch((err) => {
         console.log(err);
+        showToast("Failed to delete user! Please try again later!");
       });
   }
-  // expand object id
-  function showAllId(event) {
-    const id = event.currentTarget.dataset.id;
-    event.currentTarget.textContent = id;
-  }
+
   // Update current page state when page number is clicked
   function onPageChange(page) {
     setCurrentPage(page);
@@ -128,6 +129,40 @@ const UsersDash = () => {
       setAllUsersInPage(allUsers);
     }
   }
+
+  // expand object id
+  function showAllId(event) {
+    const id = event.currentTarget.dataset.id;
+    if (displayId) {
+      event.currentTarget.textContent = id.substring(0, 6) + "...";
+    } else {
+      event.currentTarget.textContent = id;
+    }
+    setDisplayId((prevDisplayId) => !prevDisplayId);
+  }
+
+  // expand email 
+  function showAllEmail(event,userEmail) {
+    const email = userEmail;
+    if(displayEmail){
+      event.currentTarget.textContent = hideEmail(email);
+    }else{
+      event.currentTarget.textContent = email;
+
+    }
+    setDisplayEmail((prevDisplayEmail) => !prevDisplayEmail);
+  }
+
+  // hide form the first part of the email and expand the rest of the email (domain)
+  function hideEmail(email) {
+    const parts = email.split("@");
+    const username = parts[0];
+    const domain = parts[1];
+    const firstPart = username.substring(0, username.length / 2);
+    const ellipsis = "...";
+    return `${firstPart}${ellipsis}@${domain}`;
+  }
+
   return (
     <div className={`py-4`}>
       <h4 className={`mb-2 py-3 ps-4 ${dashStyle["fw-bold"]}`}>
@@ -163,83 +198,102 @@ const UsersDash = () => {
             />
           </div>
         </div>
-        <div className="table-responsive mb-5">
-          <table className="table border-top" id="DataTables_Table_0">
-            <thead>
-              <tr>
-                <th scope="col" className="ps-4">
-                  #ID
-                </th>
-                <th scope="col">FullName</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Governorate</th>
-                <th scope="col" className="text-center">
-                  City
-                </th>
-                <th scope="col">Orders</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {allUsers.length > 0 ? (
-                allUsers.map((user) => {
-                  return (
-                    <tr key={user._id}>
-                      <td
-                        className={`ps-4`}
-                        data-id={user._id}
-                        onClick={showAllId}
-                      >
-                        {user._id.substring(0, 8) + "..."}
-                      </td>
-                      <td>{user.fullName}</td>
-                      <td>{user.email}</td>
-                      <td
-                        className={
-                          user.phone !== "" ? "text-start" : "text-center"
-                        }
-                      >
-                        {user.phone !== "" ? user.phone : "x"}
-                      </td>
-                      <td className="text-center">
-                        {user.address.governorate !== ""
-                          ? user.address.governorate
-                          : "x"}
-                      </td>
-                      <td className="text-center">
-                        {user.address.city !== "" ? user.address.city : "x"}
-                      </td>
-                      <td className="text-center">{user.order.length}</td>
-                      <td className="text-center">
-                        <FontAwesomeIcon
-                          icon={faTrashCan}
-                          type="button"
-                          onClick={() => {
-                            setUserIdToDelete(user._id);
-                            setShowWarning(true);
-                          }}
-                          className="text-danger"
-                        />
+        {!showSpinner ? (
+          <>
+            <div className="table-responsive mb-5">
+              <table className="table border-top" id="DataTables_Table_0">
+                <thead>
+                  <tr>
+                    <th scope="col" className="ps-4">
+                      #ID
+                    </th>
+                    <th scope="col">FullName</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Governorate</th>
+                    <th scope="col" className="text-center">
+                      City
+                    </th>
+                    <th scope="col">Orders</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {allUsers.length > 0 ? (
+                    allUsers.map((user) => {
+                      return (
+                        <tr key={user._id}>
+                          <th
+                            className={`ps-4  ${dashStyle["dash-prod-id-holder"]}`}
+                            data-id={user._id}
+                            onClick={showAllId}
+                          >
+                            <span
+                              className={`d-block ${dashStyle["dash-prod-id"]}`}
+                            >
+                              {user._id}
+                            </span>
+                          </th>
+
+                          <td>{user.fullName}</td>
+                          <td
+                            onClick={(e) => showAllEmail(e,user.email)}
+                            style={{ cursor: "pointer" }}
+                          >
+                             {hideEmail(user.email)}
+                            {/* {user.email} */}
+                          </td>
+                          <td
+                            className={
+                              user.phone !== "" ? "text-start" : "text-center"
+                            }
+                          >
+                            {user.phone !== "" ? user.phone : "x"}
+                          </td>
+                          <td className="text-center">
+                            {user.address.governorate !== ""
+                              ? user.address.governorate
+                              : "x"}
+                          </td>
+                          <td className="text-center">
+                            {user.address.city !== "" ? user.address.city : "x"}
+                          </td>
+                          <td className="text-center">{user.order.length}</td>
+                          <td className="text-center">
+                            <FontAwesomeIcon
+                              icon={faTrashCan}
+                              type="button"
+                              onClick={() => {
+                                setUserIdToDelete(user._id);
+                                setShowWarning(true);
+                              }}
+                              className="text-danger"
+                            />
+                          </td>
+                        </tr>
+                      );
+                    })
+                  ) : (
+                    <tr>
+                      <td colSpan="8" className="text-center">
+                        No users found.
                       </td>
                     </tr>
-                  );
-                })
-              ) : (
-                <tr>
-                  <td colSpan="8" className="text-center">
-                    No users found.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-        <DashPagination
-          totalPages={allUsersInPage.totalPages}
-          currentPage={currentPage}
-          onPageChange={onPageChange}
-        />
+                  )}
+                </tbody>
+              </table>
+            </div>
+
+            <DashPagination
+              totalPages={allUsersInPage.totalPages}
+              currentPage={currentPage}
+              onPageChange={onPageChange}
+            />
+          </>
+        ) : (
+          <Spinner />
+        )}
+
         {showWarning && userIdToDelete && (
           <ConfirmPopup
             msg={"Are you sure you want to delete user?"}
