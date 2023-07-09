4dcd455e9da22580bbb5400ed2b1731bcaf24dec
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-09 17:22:54 +0300

    CS-69 update acount order history

--- src/components/account/accountOrders.jsx
+++ src/components/account/accountOrders.jsx
@@ -15,7 +15,7 @@ const AccountOrders = ({ token }) => {
   const [userOrder, setUserOrder] = useState([]);
   const [showSpinner, setShowSpinner] = useState(true);
   const { id } = useParams();
-  
+
   useEffect(() => {
     setShowSpinner(true);
     if (jwtDecode(token).role === "user") {
@@ -61,7 +61,7 @@ const AccountOrders = ({ token }) => {
               const collapseId = `collapse-${order._id}`;
               return (
                 <div id="accordion" key={order._id} className="container mb-5">
-                  <div className="card row col-12 col-lg-9">
+                  <div className="card col-12 col-lg-9">
                     <div className={`card-header ${styles["cardHeader"]}`}>
                       <div
                         className={`d-flex flex-column flex-sm-row p-2 justify-content-sm-between align-items-sm-center ${styles.orderHeader}`}
@@ -84,8 +84,8 @@ const AccountOrders = ({ token }) => {
                         <div className="mt-3 mt-sm-0">
                           <button
                             className={`btn rounded-pill px-3 py-2 bg-white ${styles["btn-show-order"]}`}
-                            data-toggle="collapse"
-                            data-target={`${collapseId}`}
+                            data-bs-toggle="collapse"
+                            data-bs-target={`${collapseId}`}
                             aria-expanded={
                               isCollapsed[order._id] ? "false" : "true"
                             }
@@ -133,18 +133,18 @@ const AccountOrders = ({ token }) => {
                                       {item?.product_id?.brand?.name}
                                     </p>
                                   </div>
-                                  <div className="ml-2 mt-1">
+                                  <div className="mt-1">
                                     <p
                                       className={`${styles.price} py-1 px-2 text-center`}
                                     >
                                       $
                                       {item.price *
-                                        (1 - item.product_id.discount / 100)}
+                                        (1 - item.product_id.discount / 100).toFixed(2)}
                                     </p>
                                   </div>
                                 </div>
 
-                                <div className="">
+                                <div>
                                   <p className="text-start text-secondary">
                                     Qty {item.quantity}
                                   </p>
