76cde2408829cb04aa7b913c2725f7cfe90e20de
Amalhalim <amalhalim333@gmail.com>
2023-07-13 17:01:12 +0300

    modify in about page

--- src/components/about/WhatTell.jsx
+++ src/components/about/WhatTell.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-
 import styles from "../../pages/about/about.module.css";
 
 export default function WhatTell() {
   return (
-    <div className={`${styles.thecontent} container `}>
-      <h1 className={`pt-5 `}> what they're saying</h1>
+    <div className={`${styles.thecontent} container`}>
+      <h1 className={`pt-5`}> what they're saying</h1>
       <p className={`pb-3 pt-5`}>
-        " "I recently purchased a sectional sofa from Comfy, and I couldn't be
+        "I recently purchased a sectional sofa from Comfy, and I couldn't be
         happier with my experience. The process of ordering online was so easy
         and stress-free, and the customer service team was incredibly helpful
         when I had some questions about delivery. When the sofa arrived, I was
@@ -16,9 +14,9 @@ export default function WhatTell() {
         spot to relax and unwind at the end of the day. I highly recommend Comfy
         to anyone looking for stylish and comfortable furniture"
       </p>
-      <h5> Mohamad Ali</h5>
+      <h5>Mohamad Ali</h5>
       <div>
-        <h6 className=" mb-3 "> Analyst</h6>
+        <h6 className="mb-4">Analyst</h6>
       </div>
     </div>
   );
